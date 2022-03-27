6d37d5a6f724e3b4cf46433f7a459f57a3c2afd7
Tushnov Oleg <tyshnov@bk.ru>
2022-03-27 15:20:31 +0300

    Initial commit

---
+++ Client/Client/ChatForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Client
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enterChat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nicknameData = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.userList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.messageData = new System.Windows.Forms.TextBox();
+            this.userMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nameData = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterChat
+            // 
+            this.enterChat.Enabled = false;
+            this.enterChat.Location = new System.Drawing.Point(1022, 9);
+            this.enterChat.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.enterChat.Name = "enterChat";
+            this.enterChat.Size = new System.Drawing.Size(250, 51);
+            this.enterChat.TabIndex = 0;
+            this.enterChat.Text = "Подключится";
+            this.enterChat.UseVisualStyleBackColor = true;
+            this.enterChat.Click += new System.EventHandler(this.enterChat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(520, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Для подключении к чату введите свой ник: ";
+            // 
+            // nicknameData
+            // 
+            this.nicknameData.Enabled = false;
+            this.nicknameData.Location = new System.Drawing.Point(537, 11);
+            this.nicknameData.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.nicknameData.Name = "nicknameData";
+            this.nicknameData.Size = new System.Drawing.Size(475, 35);
+            this.nicknameData.TabIndex = 2;
+            // 
+            // chatBox
+            // 
+            this.chatBox.Enabled = false;
+            this.chatBox.Location = new System.Drawing.Point(16, 186);
+            this.chatBox.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(872, 576);
+            this.chatBox.TabIndex = 3;
+            this.chatBox.Text = "";
+            // 
+            // userList
+            // 
+            this.userList.Enabled = false;
+            this.userList.FormattingEnabled = true;
+            this.userList.ItemHeight = 29;
+            this.userList.Location = new System.Drawing.Point(908, 118);
+            this.userList.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(359, 700);
+            this.userList.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Чат: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(915, 83);
+            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(296, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Список пользователей: ";
+            // 
+            // messageData
+            // 
+            this.messageData.Enabled = false;
+            this.messageData.Location = new System.Drawing.Point(16, 781);
+            this.messageData.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.messageData.Name = "messageData";
+            this.messageData.Size = new System.Drawing.Size(872, 35);
+            this.messageData.TabIndex = 7;
+            this.messageData.KeyUp += new System.Windows.Forms.KeyEventHandler(this.messageData_KeyUp);
+            // 
+            // userMenu
+            // 
+            this.userMenu.ImageScalingSize = new System.Drawing.Size(36, 36);
+            this.userMenu.Name = "userMenu";
+            this.userMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // nameData
+            // 
+            this.nameData.AutoSize = true;
+            this.nameData.Location = new System.Drawing.Point(147, 101);
+            this.nameData.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.nameData.Name = "nameData";
+            this.nameData.Size = new System.Drawing.Size(79, 29);
+            this.nameData.TabIndex = 9;
+            this.nameData.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 101);
+            this.label4.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nickname:";
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1309, 915);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nameData);
+            this.Controls.Add(this.messageData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userList);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.nicknameData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enterChat);
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "ChatForm";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChatForm_FormClosing);
+            this.Load += new System.EventHandler(this.ChatForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterChat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nicknameData;
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.ListBox userList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox messageData;
+        private System.Windows.Forms.ContextMenuStrip userMenu;
+        private System.Windows.Forms.Label nameData;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Server/Server/Server.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public static class Server
+    {
+        // коллекция файлов
+        public static List<FileD> Files = new List<FileD>();
+        
+        //структура файл
+        public struct FileD
+        {
+            public int ID;
+            public string FileName;
+            public string From;
+            public int FileSize;
+            public byte[] fileBuffer;
+        }
+
+        public static int CountUsers = 0;
+        public delegate void UserEvent(string Name);
+        
+        // уведомление об подключении пользователя
+        public static event UserEvent UserConnected = (Username) =>
+        {
+            Console.WriteLine($"User {Username} connected.");
+            CountUsers++;
+            SendGlobalMessage($"Пользователь {Username} подключился к чату.", "Black");
+            SendUserList();
+        };
+
+        // уведомление об отключении пользователя
+        public static event UserEvent UserDisconnected = (Username) =>
+        {
+            Console.WriteLine($"User {Username} disconnected.");
+            CountUsers--;
+            SendGlobalMessage($"Пользователь {Username} отключился от чата.","Black");
+            SendUserList();
+        };
+
+        // коллекция пользователей
+        public static List<User> UserList = new List<User>();
+
+        // Настройка сокетов для подключения
+        public static Socket ServerSocket;
+        public const string Host = "127.0.0.1";
+        public const int Port = 2222;
+        public static bool Work = true;
+
+        // Вывод файла по id
+        public static FileD GetFileByID(int ID)
+        {
+            int countFiles = Files.Count;
+            for(int i = 0;i < countFiles;i++)
+            {
+                if (Files[i].ID == ID)
+                    return Files[i];
+            }
+            return new FileD() { ID = 0};
+        }
+        // добавление нового пользователя в коллекцию
+        public static void NewUser(User usr)
+        {
+            if (UserList.Contains(usr))
+                return;
+            UserList.Add(usr);
+            UserConnected(usr.Username);
+        }
+
+        // удаление пользователя из коллекции
+        public static void EndUser(User usr)
+        {
+            if (!UserList.Contains(usr))
+                return;
+            UserList.Remove(usr);
+            usr.End();
+            UserDisconnected(usr.Username);
+
+        }
+
+        // получить пользователя для приватного сообщения
+        public static User GetUser(string Name)
+        {
+            for(int i = 0;i < CountUsers;i++)
+            {
+                if (UserList[i].Username == Name)
+                    return UserList[i];
+            }
+            return null;
+        }
+
+        // отправка список пользователей
+        public static void SendUserList()
+        {
+            string userList = "#userlist|";
+
+            for(int i = 0;i < CountUsers;i++)
+            {
+                userList += UserList[i].Username + ",";
+            }
+
+            SendAllUsers(userList);
+        }
+
+        // отправка сообщения всем пользователям
+        public static void SendGlobalMessage(string content,string clr)
+        {
+            for(int i = 0;i < CountUsers;i++)
+            {
+                UserList[i].SendMessage(content, clr);
+            }
+        }
+
+        public static void SendAllUsers(byte[] data)
+        {
+            for(int i = 0;i < CountUsers;i++)
+            {
+                UserList[i].Send(data);
+            }
+        }
+        public static void SendAllUsers(string data)
+        {
+            for (int i = 0; i < CountUsers; i++)
+            {
+                UserList[i].Send(data);
+            }
+        }
+
+
+    }
+}
